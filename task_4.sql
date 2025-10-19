1fb5caae2f53dae7764718625caad2e33c2167ae
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-10-20 00:56:11 +0200

    Minor modifications

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
-SELECT *
+SELECT COLUMN_NAME, COLUMN_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE table_name = 'Books'
-  AND table_schema = 'alx_book_store';
+WHERE TABLE_NAME = 'Books'
+  AND TABLE_SCHEMA = 'alx_book_store';
 
