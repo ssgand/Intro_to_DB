d1bdaa1b3b8ab6d97326c88ec5aeb03035214ddb
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-10-20 00:32:02 +0200

    New file to start the new database but from a python file

---
+++ MySQLServer.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+try:
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="alx_book_store"
+  )
+
+except:
+  print('Failed to connect to the DB')
+
+else:
+  mycursor = mydb.cursor()
+
+  mycursor.execute("""
+    CREATE DATABASE IF NOT EXISTS alx_book_store;
+  """)
+
+  print("Database 'alx_book_store'  created successfully!")
+
+  mycursor.close()
+  mydb.close()
